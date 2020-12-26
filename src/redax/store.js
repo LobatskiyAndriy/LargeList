76c3f8ebe6b7c74a284e8a25fdd39d9bf66282e9
Lobatskiy Andriy <lobatskiy31@gmail.com>
2020-12-26 14:54:12 +0200

    debag

--- src/redax/store.js
+++ src/redax/store.js
@@ -4,9 +4,9 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers'
 
 
-const store = createStore(rootReducer, compose(applyMiddleware(thunk),
+const store = createStore(rootReducer, 
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-));
+);
 
 export default store;
 
